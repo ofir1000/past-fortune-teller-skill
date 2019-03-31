698d2f3bb137f06aaec4770bcc89430bcdb0895c
ofir1000 <ofir4buying@gmail.com>
2019-03-31 21:17:40 +0000

    added question for asking question

--- __init__.py
+++ __init__.py
@@ -1,13 +1,25 @@
 from mycroft import MycroftSkill, intent_file_handler
-
+from adapt.intent import IntentBuilder
+from mycroft.skills.core import MycroftSkill
+from mycroft.util.log import getLogger
+from mycroft.skills.context import adds_context, removes_context
 
 class PastFortuneTeller(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
 
     @intent_file_handler('teller.fortune.past.intent')
+    @adds_context('YesContext')
     def handle_teller_fortune_past(self, message):
-        self.speak_dialog('teller.fortune.past')
+        self.answer = false
+        self.speak_dialog('teller.fortune.past', expect_response=True)
+
+    @intent_handler(IntentBuilder('AnwerIntent').require("YesKeyword").
+                                  require('YesContext').build())
+    @remove_context('YesContext')
+    @adds_context('NameContext')
+    def handle_answer_intent(self, message):
+        self.speak('O.K. please tell me your name?', expect_response=True)
 
 
 def create_skill():
